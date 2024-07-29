72f5a7a4305d47d88dfcbc9dd401be0a5a246a17
Gautham P Harish <your-gauthampharish@gmail.com>
2024-07-29 09:15:17 +0530

    Update .gitignore to remove tracked files

--- alertsmanagementserv/apps.py
+++
@@ -1,9 +0,0 @@
-# pricetracker/alertsmanagementserv/apps.py
-
-from django.apps import AppConfig
-
-
-class AlertsManagementServConfig(AppConfig):
-    name = 'alertsmanagementserv'
-
-
--- identitymanagementserv/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class IdentitymanagementservConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'identitymanagementserv'
-
-    def ready(self):
-        import identitymanagementserv.signals
